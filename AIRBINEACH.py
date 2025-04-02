e89a98409908c59ed1a02c4bf487d76170d68bc0
Camila Pereira <camilapereirasp96@gmail.com>
2025-04-02 15:21:41 -0300

    Update AIRBINEACH.py

--- AIRBINEACH.py
+++ AIRBINEACH.py
@@ -1,22 +1,16 @@
-from Funcao import menu, cadastroimovel, voltarmenu, alugarimovel
+from Funcao import listar_imoveis, menu, cadastroimovel, alugarimovel, voltarmenu
 
 while True:
     opcao = menu()
-    
     if opcao == 1:
         cadastroimovel()
-        resp = input("Deseja cadastrar mais algum imóvel? (sim/não) ").lower()
-        while resp == "sim":
-            cadastroimovel()
-            resp = input("Deseja cadastrar mais algum imóvel? (sim/não) ").lower()
     elif opcao == 2:
+        listar_imoveis()
         alugarimovel()
+        if voltarmenu():
+            continue
     elif opcao == 0:
         print("Obrigada por usar nossos serviços!")
         break
     else:
         print("Opção inválida, tente novamente.")
-    
-    if not voltarmenu():
-        print("Fechando o programa... Até logo!")
-        break
