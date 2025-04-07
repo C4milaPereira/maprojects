e2835d7edf5e2969e72e782e6c3a0ffde8ca1ef1
Camila Pereira <camilapereirasp96@gmail.com>
2025-04-07 17:36:00 -0300

    Update MARBNB.py

--- AIRBINEACH.py
+++ AIRBINEACH.py
@@ -1,16 +1,22 @@
-from Funcao import listar_imoveis, menu, cadastroimovel, alugarimovel, voltarmenu
+import os
+
+from Funcao import listar_imoveis, menu, cadastroimovel, alugarimovel, saudacao, voltarmenu
 
 while True:
     opcao = menu()
     if opcao == 1:
         cadastroimovel()
+        if not voltarmenu():
+            saudacao()
+            break
     elif opcao == 2:
         listar_imoveis()
         alugarimovel()
-        if voltarmenu():
-            continue
+        if not voltarmenu():
+            saudacao()
+            break
     elif opcao == 0:
-        print("Obrigada por usar nossos serviços!")
+        saudacao()
         break
     else:
         print("Opção inválida, tente novamente.")
